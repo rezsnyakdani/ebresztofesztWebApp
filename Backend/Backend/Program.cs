050dee2bfd538b1fede06f8537b7605ec19f0370
rezsnyakdani <rezsnyakd@gmail.com>
2026-06-04 23:06:20 +0200

    Update appsetting.json & program.cs with database

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,3 +1,5 @@
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend
 {
@@ -9,6 +11,8 @@ namespace Backend
 
             // Add services to the container.
 
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
