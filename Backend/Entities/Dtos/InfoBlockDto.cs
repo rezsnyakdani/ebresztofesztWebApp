0683422bd795d2fe050df9dcefa764ed500e200a
rezsnyakdani <rezsnyakd@gmail.com>
2026-06-10 11:09:30 +0200

    Add INfoBlockDto & InfoBlockLogic & InfoBlockCoontroller

---
+++ Backend/Entities/Dtos/InfoBlockDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos
+{
+    public class InfoBlockDto
+    {
+        public string Title { get; set; } = null!;
+        public string Content { get; set; } = null!;
+        public int OrderIndex { get; set; }
+    }
+}
