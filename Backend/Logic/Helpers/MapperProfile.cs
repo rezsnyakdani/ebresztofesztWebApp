7db812a0defd255b9e8f6c79b08275fceb906c3a
rezsnyakdani <rezsnyakd@gmail.com>
2026-06-10 11:12:38 +0200

    Add MapperProfile

---
+++ Backend/Logic/Helpers/MapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Dtos;
+using Entities.Models;
+using AutoMapper;
+
+namespace Logic.Helpers
+{
+    public class MapperProfile : AutoMapper.Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<InfoBlock, InfoBlockDto>().ReverseMap();
+        }
+    }
+}
