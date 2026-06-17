3f98dc1d9bbf69b2cb800b509687a81770927f91
rezsnyakdani <rezsnyakd@gmail.com>
2026-06-17 22:38:42 +0200

    Add PasswordHasher

---
+++ Backend/Logic/Helpers/PasswordHasher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Helpers
+{
+    public static class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            using var sha256 = SHA256.Create();
+            var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return Convert.ToBase64String(hashedBytes);
+        }
+    }
+}
