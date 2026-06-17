e80a61cf9d0fbeb5408e0da14fe9a541e21bfeca
rezsnyakdani <rezsnyakd@gmail.com>
2026-06-17 21:40:43 +0200

    Add dbseeder

---
+++ Backend/Data/DbSeeder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Entities.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Backend.Data
+{
+    public static class DbSeeder
+    {
+        public static void SeedAdminUser(IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            if (!context.Profiles.Any(p => p.Id == "admin"))
+            {
+                var admin = new Profile
+                {
+                    Id = "admin",
+                    Name = "admin",
+                    Email = "admin@gmail.com",
+                    PasswordHash = "admin",
+                    Role = "Szervező",
+                    BirthDate = new DateTime(2000, 1, 1),
+                    Gender = "admin"
+                };
+
+                context.Profiles.Add(admin);
+                context.SaveChanges();
+            }
+        }
+    }
+}
