1dc4e619f665bd9147c0b4ad8c00b2f95a38929a
rezsnyakdani <rezsnyakd@gmail.com>
2026-06-18 22:24:24 +0200

    Create SongController & add SongLogic to Program.cs

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -31,6 +31,7 @@ namespace Backend
             builder.Services.AddScoped<InfoBlockLogic>();
             builder.Services.AddScoped<LectureLogic>();
             builder.Services.AddScoped<ProgramItemLogic>();
+            builder.Services.AddScoped<SongLogic>();
             builder.Services.AddProblemDetails();
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
