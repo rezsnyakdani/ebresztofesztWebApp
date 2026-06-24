8922534e3c41337fc8b29985ca221e6d91f33ae7
rezsnyakdani <rezsnyakd@gmail.com>
2026-06-24 11:09:51 +0200

    Add admin-navbar logic

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -10,6 +10,11 @@ import { LoginComponent } from './features/login/login.component';
 import { EloadasComponent } from './features/eloadas/eloadas.component';
 import { InfoComponent } from './features/info/info.component';
 import { AdminProfilComponent } from './features/szervezoknek/admin-profil/admin-profil.component';
+import { AdminDalComponent } from './features/szervezoknek/admin-dal/admin-dal.component';
+import { AdminEloadasComponent } from './features/szervezoknek/admin-eloadas/admin-eloadas.component';
+import { AdminInfoComponent } from './features/szervezoknek/admin-info/admin-info.component';
+import { AdminMuhelyComponent } from './features/szervezoknek/admin-muhely/admin-muhely.component';
+import { AdminProgramComponent } from './features/szervezoknek/admin-program/admin-program.component';
 
 
 const routes: Routes = [
@@ -22,6 +27,11 @@ const routes: Routes = [
   { path: 'eloadas', component: EloadasComponent },
   { path: 'info', component: InfoComponent },
   { path: 'admin-profil', component: AdminProfilComponent },
+  { path: 'admin-dal', component: AdminDalComponent },
+  { path: 'admin-eloadas', component: AdminEloadasComponent },
+  { path: 'admin-info', component: AdminInfoComponent },
+  { path: 'admin-muhely', component: AdminMuhelyComponent },
+  { path: 'admin-program', component: AdminProgramComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' }
 ];
