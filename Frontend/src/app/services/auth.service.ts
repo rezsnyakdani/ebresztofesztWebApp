49c596179e61d7fd822f844759d9c794bf4e1910
rezsnyakdani <rezsnyakd@gmail.com>
2026-06-29 11:34:18 +0200

    fix token decode

--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -64,9 +64,16 @@ export class AuthService {
 
     try {
       const payload = token.split('.')[1];
-      const decodedPayload = atob(payload.replace(/-/g, '+').replace(/_/g, '/'));
-      return JSON.parse(decodedPayload);
+      const base64 = payload.replace(/-/g, '+').replace(/_/g, '/');
+      
+      const jsonPayload = decodeURIComponent(
+        window.atob(base64).split('').map(function(c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join('')
+      );
+      return JSON.parse(jsonPayload);
     } catch (e) {
+      console.error('Hiba a token dekódolásakor:', e);
       return null;
     }
   }
