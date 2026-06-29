c488f5e35c956683996b699c63143ca9735701d4
rezsnyakdani <rezsnyakd@gmail.com>
2026-06-29 10:52:37 +0200

    Add login function

--- Frontend/src/app/features/login/login.component.ts
+++ Frontend/src/app/features/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +9,45 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.sass'
 })
 export class LoginComponent {
+  name = '';
+  password = '';
 
+  errorMessage = '';
+  successMessage = '';
+  isLoading = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  login(): void {
+    this.errorMessage = '';
+    this.successMessage = '';
+
+    if (!this.name || !this.password) {
+      this.errorMessage = 'Kérlek, tölts ki minden mezőt!';
+      return;
+    }
+
+    this.isLoading = true;
+
+    this.authService.login({ name: this.name, password: this.password }).subscribe({
+      
+      next: () => {
+        this.successMessage = 'Sikeres bejelentkezés! Átirányítás...';
+        
+        setTimeout(() => {
+          this.isLoading = false;
+          this.router.navigate(['/profil']);
+        }, 1000);
+      },
+
+      error: (err) => {
+        this.isLoading = false; 
+        
+        this.errorMessage = err.message; 
+      }
+    });
+  }
 }
