0937ef2785a3abdf55d3e0ce930fbc745aabb653
rezsnyakdani <rezsnyakd@gmail.com>
2026-06-29 09:23:16 +0200

    add authservice

--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError, BehaviorSubject } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+import { parseErrorMessage } from '../helpers/error-parser.helper';
+
+export interface LoginDto {
+  name: string;
+  password: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = environment.apiUrl;
+  
+  private tokenKey = 'festival_auth_token';
 
-  constructor() { }
+
+  private loggedInSubject = new BehaviorSubject<boolean>(this.hasToken());
+  public isLoggedIn$ = this.loggedInSubject.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  login(credentials: LoginDto): Observable<any> {
+    return this.http.post<{ token: string }>(`${this.apiUrl}/Auth/login`, credentials).pipe(
+      tap(response => {
+        if (response && response.token) {
+          this.saveToken(response.token);
+          this.loggedInSubject.next(true);
+        }
+      }),
+      catchError((error: HttpErrorResponse) => {
+        const tisztaHiba = parseErrorMessage(error);
+        return throwError(() => new Error(tisztaHiba));
+      })
+    );
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.tokenKey);
+    this.loggedInSubject.next(false);
+  }
+
+  private saveToken(token: string): void {
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+  public getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  private hasToken(): boolean {
+    return !!this.getToken();
+  }
 }
