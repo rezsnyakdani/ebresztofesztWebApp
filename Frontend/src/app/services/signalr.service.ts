74808e73a3f4405f8ffcb3271a3f04f1c26ffccf
rezsnyakdani <rezsnyakd@gmail.com>
2026-06-30 12:00:30 +0200

    Add signalr to frontend

---
+++ Frontend/src/app/services/signalr.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import * as signalR from '@microsoft/signalr';
+import { Subject } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class SignalrService {
+  private hubConnection!: signalR.HubConnection;
+
+  public workshopsChanged$ = new Subject<void>();
+  public profilesChanged$ = new Subject<void>();
+
+  startConnection(): void {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl(environment.hubUrl)
+      .withAutomaticReconnect()
+      .build();
+
+    this.registerListeners();
+
+    this.hubConnection
+      .start()
+      .then(() => console.log('SignalR kapcsolat sikeresen létrejött.'))
+      .catch(err => console.error('SignalR kapcsolat sikertelen:', err));
+  }
+
+  private registerListeners(): void {
+    this.hubConnection.on('WorkshopsChanged', () => {
+      console.log('SignalR: WorkshopsChanged esemény érkezett.');
+      this.workshopsChanged$.next();
+    });
+
+    this.hubConnection.on('ProfilesChanged', () => {
+      console.log('SignalR: ProfilesChanged esemény érkezett.');
+      this.profilesChanged$.next();
+    });
+  }
+
+  stopConnection(): void {
+    this.hubConnection?.stop();
+  }
+}
